ca2ab852c72ed509bb455e3ffe013813b2f305f0
Georgi Nikolov <ggeorgi60@gmail.com>
2025-04-02 18:02:45 +0000

    Added a basic raylib window

--- source/main.cpp
+++ source/main.cpp
@@ -2,5 +2,23 @@
 #include <raylib.h>
 
 int main() {
-    std::cout << "This is a message!\n";
+    // TODO: Add these constants to a constants sheet with all
+    // of the global settings
+    const int   SCREEN_WIDTH  = 800;
+    const int   SCREEN_HEIGHT = 450;
+    const int   TARGET_FPS    = 60;
+    const char* TITLE         = "basic window";
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
+    SetTargetFPS(TARGET_FPS);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("This is a basic raylib window", 190, 200, 20, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
