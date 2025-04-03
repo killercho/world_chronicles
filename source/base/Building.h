058adfbc1a66ba31952ebbb917121fb0571935be
Georgi Nikolov <ggeorgi60@gmail.com>
2025-04-03 17:31:35 +0000

    Added basic values into the building base class and prepared a space for all of the building types

--- source/base/Building.h
+++ source/base/Building.h
@@ -5,21 +5,44 @@
 // All of the buildings in the game will inherit from this class
 
 class Building {
- private:
-    // TODO: Add the rest of the fields that the base class should have
+ protected:
+    // TODO: Add fields for the special abilities
+    // TODO: Add an array with the technologies inside
+    // TODO: Add an array with the cost to maintain
+
+    // Health that the unit has currently
     int m_health;
 
+    // Defense that the building has, used when other entities are attacking
+    int m_defense;
+
+    // Area that the building can use ranged attacks if there are any
+    unsigned int rangedArea;
+
+    // Area in which the unit cannot attack
+    // because the other entity is too close
+    unsigned int noAttackArea;
+
  public:
+    Building()                           = default;
+    virtual ~Building()                  = default;
     Building(const Building&)            = default;
     Building(Building&&)                 = default;
     Building& operator=(const Building&) = default;
     Building& operator=(Building&&)      = default;
 
-    // TODO: Add all of the base functions that the base class should have like:
-    //  Destroy() - that plays the destroy animation
-    //  Build() - that plays the destroy animation
-    //  ShowUI() - that plays the destroy animation
-    //  ...
+    // Plays the destroy animation and destroys the building
+    // leaving a mark
+    virtual void Destroy() = 0;
+
+    // Plays the building animation with all of the building specifications like time to build
+    virtual void Build() = 0;
+
+    // Shows the UI of the current building allowing for technology research or unit creation
+    virtual void ShowUI() = 0;
+
+    // Attacks another entity
+    virtual void Attack() = 0;
 };
 
 #endif  // SOURCE_BASE_BUILDING_H_
---
+++ source/buildings/BuildingConstants.h
@@ -0,0 +1,9 @@
+#ifndef SOURCE_BUILDINGS_BUILDINGCONSTANTS_H_
+#define SOURCE_BUILDINGS_BUILDINGCONSTANTS_H_
+
+// Constants regarding the castle building
+namespace castle {
+const int MAX_HEALTH = 100;
+}
+
+#endif  // SOURCE_BUILDINGS_BUILDINGCONSTANTS_H_
