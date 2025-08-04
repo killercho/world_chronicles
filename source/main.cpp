64566a01671b69bdc8dc18e12f33231f184acb35
Georgi Nikolov <ggeorgi60@gmail.com>
2025-08-04 17:42:38 +0000

    Added the raygui library
    This library will be responsible for the interface in the game

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,11 @@
 #include <Constants.h>
 #include <buildings/Castle.h>
 
+#include <raygui/raygui.h>
 #include <raylib.h>
 
 #include <memory>
+#include <string>
 
 int main() {
     InitWindow(general_settings::SCREEN_WIDTH,
@@ -14,10 +16,17 @@ int main() {
     // TODO: Create a basic castle and check the spawning and UI
     // std::shared_ptr<Castle> castleTest = std::make_shared<Castle>();
 
+    std::string curr = "Label is empty";
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("This is a basic raylib window", 190, 200, 20, BLACK);
+
+        GuiLabel(Rectangle(100, 100, 50, 50), curr.c_str());
+        if (GuiButton(Rectangle(50, 50, 50, 50), "Click to change the text!")) {
+            curr = "Label should be full now";
+        }
+
         EndDrawing();
     }
 
