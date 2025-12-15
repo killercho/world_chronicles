660a2bb7d62db4bc2ab3dfcbca68b24848228960
Georgi Nikolov <ggeorgi60@gmail.com>
2025-12-15 18:16:11 +0000

    Added a basic structure for the ini files that will hold the settings of the game

--- source/Constants.h
+++ source/Constants.h
@@ -6,6 +6,10 @@ const int SCREEN_HEIGHT = 576;
 const int TARGET_FPS    = 60;
 
 const char* const TITLE = "World Chronicles";
+
+// TODO: Add a constants class that is supposed to parse
+// the ini files and store the settings in them.
+
 }  // namespace general_settings
 
 #endif  // SOURCE_CONSTANTS_H_
